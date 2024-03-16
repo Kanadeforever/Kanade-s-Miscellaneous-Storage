82a24e0455e8603fc9fbd9521fc04a2a2c0aafa4
Kanadeforever <kanadeforever@outlook.com>
2024-03-16 09:25:41 +0800

    Update Launcher With ISO.bat

--- Batch/Launcher With ISO.bat
+++ Batch/Launcher With ISO.bat
@@ -7,19 +7,22 @@ rem 使用Powershell挂载ISO镜像
 PowerShell -Command Mount-DiskImage -ImagePath '%CurDir%\Image.iso'
 
 rem 执行程序
-start Program.exe
+start YourAppName1.exe
 
 rem 检查程序是否运行
-set taskname=Program.exe
-:a
-set a=
-tasklist|find /i "%taskname%">nul 2>nul&&set b==||set a==
+:step_a
+tasklist | find /i "YourAppName1.exe" > nul
+set process1_exist=%errorlevel%
 
-rem 如果程序没有运行，则卸载镜像
-if "%a%%b%"=="==" goto:b
-ping 127.1 -n 2 >nul
-goto:a
-:b
+rem 如果进程存在，则3秒后继续循环检测
+if %process1_exist% equ 0 (
+    timeout /t 3
+    goto step_a
+) else (
+    goto step_b
+)
+
+:step_b
 PowerShell -Command Dismount-DiskImage -ImagePath '%CurDir%\Image.iso'
 exit
 @echo off
