827124fd0dd15c7d089428c154b7f8319ef0283e
Kanadeforever <kanadeforever@outlook.com>
2024-03-16 10:10:19 +0800

    Update Launcher With ISO.bat

--- Batch/Launcher With ISO.bat
+++ Batch/Launcher With ISO.bat
@@ -9,15 +9,26 @@ PowerShell -Command Mount-DiskImage -ImagePath '%CurDir%\Image.iso'
 rem 执行程序
 start YourAppName1.exe
 
+timeout /t 5
+
 rem 检查程序是否运行
 :step_a
 tasklist | find /i "YourAppName1.exe" > nul
-set process1_exist=%errorlevel%
+if %errorlevel% equ 0 (
+        timeout /t 3
+        cls
+) else (
 
-rem 如果进程存在，则3秒后继续循环检测
-if %process1_exist% equ 0 (
-    timeout /t 3
-    goto step_a
+tasklist | find /i "YourAppName2.exe" > nul
+if %errorlevel% equ 0 (
+        timeout /t 3
+        cls
+) else (
+
+tasklist | find /i "YourAppName3.exe" > nul
+if %errorlevel% equ 0 (
+        timeout /t 3
+        cls
 ) else (
     goto step_b
 )
